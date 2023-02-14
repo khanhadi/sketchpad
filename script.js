747494aceaed6733a2b5473342cbe23a5f26a7c0
Hadi <104451011+khanhadi@users.noreply.github.com>
2023-02-14 18:53:18 +0300

    Implement drawing functions based on mode selected

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 // TOOLBAR
+
+// modes
+const clickBtn = document.querySelector('#click-btn');
+const hoverBtn = document.querySelector('#hover-btn');
+
+let isClick = false;
+let isHover = false;
+
+clickBtn.addEventListener('click', () => {
+	clickBtn.classList.add('selected');
+	hoverBtn.classList.remove('selected');
+	isClick = true;
+	isHover = false;
+	console.log('Click Mode');
+	enableDrawing(isHover, isClick);
+});
+
+hoverBtn.addEventListener('click', () => {
+	hoverBtn.classList.add('selected');
+	clickBtn.classList.remove('selected');
+	isClick = false;
+	isHover = true;
+	console.log('Hover Mode');
+	enableDrawing(isHover, isClick);
+});
+
+// color-picker
 let currentColor = '#000000'; // hex black
 
 const colorPicker = document.querySelector('#color-picker');
@@ -19,10 +46,38 @@ for (let i = 0; i < pixelCount * pixelCount; i++) {
 	gridContainer.appendChild(pixelDiv);
 }
 
-const pixels = document.querySelectorAll('.pixel');
+// drawing to the grid
+function enableDrawing(isHover, isClick) {
+	const pixels = document.querySelectorAll('.pixel');
 
-pixels.forEach((pixel) => {
-	pixel.addEventListener('mouseover', () => {
-		pixel.style.backgroundColor = currentColor;
-	});
-});
+	// hover mode
+	if (isHover) {
+		pixels.forEach((pixel) => {
+			pixel.removeEventListener('click', draw);
+			pixel.addEventListener('mouseover', draw);
+		});
+	}
+
+	// click and drag mode
+	if (isClick) {
+		pixels.forEach((pixel) => {
+			pixel.removeEventListener('mouseover', draw);
+		});
+
+		window.addEventListener('mousedown', () => {
+			pixels.forEach((pixel) => {
+				pixel.addEventListener('mouseover', draw);
+			});
+		});
+
+		window.addEventListener('mouseup', () => {
+			pixels.forEach((pixel) => {
+				pixel.removeEventListener('mouseover', draw);
+			});
+		});
+	}
+}
+
+function draw() {
+	this.style.backgroundColor = currentColor;
+}
