4cd304afb634e50d4a73b5da4436fee64eb7128c
Hadi <104451011+khanhadi@users.noreply.github.com>
2023-02-16 16:57:42 +0300

    Add data attr. for shade values and eraser tool

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-// TOOLBAR
+let currentColor = '#000000'; // default color
+
+//#region TOOLBAR
 
 // modes
 const clickBtn = document.querySelector('#click-btn');
@@ -6,6 +8,7 @@ const hoverBtn = document.querySelector('#hover-btn');
 
 let isClick = false;
 let isHover = false;
+let isSketch = false;
 
 clickBtn.addEventListener('click', () => {
 	clickBtn.classList.add('selected');
@@ -19,32 +22,69 @@ clickBtn.addEventListener('click', () => {
 hoverBtn.addEventListener('click', () => {
 	hoverBtn.classList.add('selected');
 	clickBtn.classList.remove('selected');
-	isClick = false;
 	isHover = true;
+	isClick = false;
 	console.log('Hover Mode');
 	enableDrawing(isHover, isClick);
+});
+
+// tools
+
+// eraser
+let erase = false;
+const eraserBtn = document.querySelector('#eraser-btn');
+eraserBtn.addEventListener('click', () => {
+	eraserBtn.classList.toggle('selected');
+
+	if (eraserBtn.classList.contains('selected')) {
+		erase = true;
+		sketch = false;
+		sketchBtn.classList.remove('selected');
+	}
+
+	if (!eraserBtn.classList.contains('selected')) {
+		erase = false;
+	}
+});
+
+// sketch
+let sketch = false;
+let shading = 0;
+const sketchBtn = document.querySelector('#sketch-btn');
+sketchBtn.addEventListener('click', () => {
+	sketchBtn.classList.toggle('selected');
+
+	if (sketchBtn.classList.contains('selected')) {
+		sketch = true;
+		erase = false;
+		eraserBtn.classList.remove('selected');
+		console.log(sketch);
+	}
+
+	if (!sketchBtn.classList.contains('selected')) {
+		sketch = false;
+		console.log(sketch);
+	}
 });
 
 // grid size slider
 const slider = document.querySelector('.slider');
 const gridSizeText = document.querySelector('.grid-size');
-
 gridSizeText.textContent = slider.value + 'x' + slider.value;
-
 slider.addEventListener('input', () => {
 	gridSizeText.textContent = slider.value + 'x' + slider.value;
 });
 
 // color-picker
-let currentColor = '#000000'; // hex black
-
 const colorPicker = document.querySelector('#color-picker');
 colorPicker.addEventListener('change', (e) => {
 	currentColor = e.target.value;
 	console.log(currentColor);
 });
 
-// GRID
+//#endregion
+
+//#region GRID
 const gridContainer = document.querySelector('.grid-container');
 
 // initialise default grid
@@ -55,6 +95,8 @@ gridContainer.style.gridTemplateColumns = 'repeat(' + pixelCount + ', 1fr)';
 for (let i = 0; i < pixelCount * pixelCount; i++) {
 	const pixelDiv = document.createElement('div');
 	pixelDiv.classList.add('pixel');
+	pixelDiv.setAttribute('draggable', false);
+	pixelDiv.setAttribute('data-shading', 0);
 	gridContainer.appendChild(pixelDiv);
 }
 
@@ -69,9 +111,11 @@ slider.addEventListener('input', () => {
 	for (let i = 0; i < pixelCount * pixelCount; i++) {
 		const pixelDiv = document.createElement('div');
 		pixelDiv.classList.add('pixel');
+		pixelDiv.setAttribute('draggable', false);
+		pixelDiv.setAttribute('data-shading', 0);
 		gridContainer.appendChild(pixelDiv);
 	}
-	enableDrawing(isHover, isClick); // preserve mode.
+	enableDrawing(isHover, isClick); // preserve mode
 });
 
 // drawing to the grid
@@ -90,6 +134,7 @@ function enableDrawing(isHover, isClick) {
 	if (isClick) {
 		pixels.forEach((pixel) => {
 			pixel.removeEventListener('mouseover', draw);
+			pixel.addEventListener('click', draw);
 		});
 
 		window.addEventListener('mousedown', () => {
@@ -107,5 +152,35 @@ function enableDrawing(isHover, isClick) {
 }
 
 function draw() {
-	this.style.backgroundColor = currentColor;
+	if (sketch) {
+		nextVal = this.dataset.shading; // get current shade val
+		if (nextVal <= 8) {
+			nextVal++;
+		}
+		this.setAttribute('data-shading', nextVal);
+		console.log(shadeUp('#000000', nextVal));
+		this.style.backgroundColor = shadeUp('#000000', nextVal);
+	} else if (erase) {
+		this.setAttribute('data-shading', 0); // reset shading.
+		this.style.backgroundColor = '#FFFFFF';
+	} else {
+		this.style.backgroundColor = currentColor;
+	}
 }
+
+function shadeUp(color, shade) {
+	return 'rgb(' + hexToRgb(color).toString() + ',' + shade * 0.15 + ')';
+}
+
+// thank u stackoverflow :p
+function hexToRgb(hex) {
+	const normal = hex.match(/^#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/i);
+	if (normal) return normal.slice(1).map((e) => parseInt(e, 16));
+
+	const shorthand = hex.match(/^#([0-9a-f])([0-9a-f])([0-9a-f])$/i);
+	if (shorthand) return shorthand.slice(1).map((e) => 0x11 * parseInt(e, 16));
+
+	return null;
+}
+
+//#endregion
